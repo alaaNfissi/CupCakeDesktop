d5ae9fd4ebfae9120c958866b482092973dbbe64
Alaa NFISSI <alaa.nfissi@esprit.tn>
2018-02-23 03:00:37 +0100

    class client

---
+++ src/tn/esprit/cupcake/entities/Client.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.esprit.cupcake.entities;
+
+import java.util.Date;
+import tn.esprit.cupcake.entities.Utilisateur;
+
+/**
+ *
+ * @author Basly
+ */
+public class Client extends Utilisateur {
+    private String adresse ;
+    private String sexe ;
+    private String image;
+
+    public Client() {
+    }
+
+    public Client(long id_utilisateur, String password, String email, String pseudo, int etat_compte, int num_tel, String nom, String prenom, String date_naissance, int role) {
+        super(id_utilisateur, password, email, pseudo, etat_compte, num_tel, nom, prenom, date_naissance, role);
+    }
+
+//    public Client(String adresse, String sexe, String image) {
+//        this.adresse = adresse;
+//        this.sexe = sexe;
+//        this.image = image;
+//    }
+    
+
+    public Client(String adresse, String sexe, String image, int id_utilisateur, String password, String email, String pseudo, int etat_compte, int num_tel, String nom, String prenom, String date_naissance, int role) {
+        super(id_utilisateur, password, email, pseudo, etat_compte, num_tel, nom, prenom, date_naissance, role);
+        this.adresse = adresse;
+        this.sexe = sexe;
+        this.image = image;
+    }
+      public Client(String password, String email, String pseudo, int etat_compte, int num_tel, String nom, String prenom, String date_naissance,int role, String adresse, String sexe, String image) {
+        super(password, email, pseudo, etat_compte, num_tel, nom, prenom, date_naissance, role);
+        this.adresse = adresse;
+        this.sexe = sexe;
+        this.image = image;
+    }
+
+    public Client(String password,String email, String pseudo, int num_tel, String nom, String prenom, String date_naissance,String adresse,String sexe) {
+        super(password,email, pseudo, num_tel, nom, prenom, date_naissance);
+        this.adresse = adresse;
+        this.sexe=sexe ;
+
+    }
+    
+    public Client(String email, String pseudo, int num_tel, String nom, String prenom, String date_naissance,String adresse,String sexe) {
+        super(email, pseudo, num_tel, nom, prenom, date_naissance);
+        this.adresse = adresse;
+        this.sexe = sexe ;
+    }
+  public Client(String password,String email, String pseudo, int num_tel, String nom, String prenom, String date_naissance,String adresse,String sexe,String image) {
+        super(password,email, pseudo, num_tel, nom, prenom, date_naissance);
+        this.adresse = adresse;
+        this.sexe = sexe ;
+        this.image = image ;
+    }
+
+
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getSexe() {
+        return sexe;
+    }
+
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" + "adresse=" + adresse + ", sexe=" + sexe + ", image=" + image + '}';
+    }
+
+    
+    
+    
+    
+}
